f8b70dd5bc96e3548a057f687a3f8de02cc8de07
saronKB <saronkiflu3@gmail.com>
2025-06-14 22:54:25 +0300

    Time: 72 ms (79.06%) | Memory: 112.8 MB (75.00%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    const int dirs[5] = {-1, 0, 1, 0, -1};
+
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j] == 1) {
+                    ans[i][j] = 0;
+                    q.emplace(i, j);
+                }
+            }
+        }
+
+        // Multi-source BFS
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+            for (int k = 0; k < 4; ++k) {
+                int x = i + dirs[k], y = j + dirs[k + 1];
+                if (x >= 0 && x < m && y >= 0 && y < n && ans[x][y] == -1) {
+                    ans[x][y] = ans[i][j] + 1;
+                    q.emplace(x, y);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
