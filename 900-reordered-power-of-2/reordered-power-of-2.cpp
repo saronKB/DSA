be5f047c91cc8f7f607d90184296eb787c937292
saronKB <saronkiflu3@gmail.com>
2025-06-14 17:13:06 +0300

    Time: 2 ms (39.54%) | Memory: 9.6 MB (13.07%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int N) {
+        unordered_set<string> powerOfTwos;
+        for (int i = 0; i < 32; ++i) {
+            int n = 1 << i;
+            string s = to_string(n);
+            sort(s.begin(), s.end());
+            powerOfTwos.insert(s);
+        }
+        string s = to_string(N);
+        sort(s.begin(), s.end());
+        return powerOfTwos.count(s) > 0;
+    }
+};
